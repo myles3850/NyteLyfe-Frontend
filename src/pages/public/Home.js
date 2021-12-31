bc759dde8fcdecb1fa8048d8b952663126de521a
myles3850 <myles@browne.link>
2021-12-31 23:23:49 +0000

    got reading and writing to supabase online

--- src/pages/public/Home.js
+++ src/pages/public/Home.js
@@ -1,34 +1,29 @@
 import { useState } from "react";
-import { createClient } from "@supabase/supabase-js";
+import { supabase } from "../../components/logic/Supabase";
+
 
 function Home() {
-	const supabaseUrl = "https://mojptfpacmyjglncbykp.supabase.co";
-	const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
-
-	const supabase = createClient(supabaseUrl, supabaseKey);
-
 	const UUID = "20cf2574-ec52-4f61-a0b9-841769a31aa0";
-
-	const [user, Setuser] = useState(10);
 
 	async function readData() {
 		let { data: Medication, error } = await supabase
-			.from("Medication")
+			.from("dosage_history")
 			.select("*");
-		console.log(Medication);
+		console.log(Medication, error);
 	}
 
-	async function updateData(user) {
+	async function updateData() {
 		const { data, error } = await supabase
 			.from("Medication")
-			.insert([{ linked_user: UUID, dose: user }]);
-		Setuser++;
+			.insert([{ dose: 10, linked_user: UUID }]);
+			console.log(data);
 	}
 
 	return (
 		<div>
 			<h1>Pill Poppers Website</h1>
-			<button onClick={readData}>press to add data to DB</button>
+			<button onClick={readData}>press to read data from DB</button>
+			<button onClick={updateData}>press to add data to DB</button>
 			<p>
 				Welcome to my new pill management page, here i will be managing
 				the pills i've taken in a format that works for those that say
