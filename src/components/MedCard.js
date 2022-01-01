c5a7f9acea8ccad1df557c974b8173d9a40edb51
myles3850 <myles@browne.link>
2022-01-01 01:05:38 +0000

    modified pages to show dummy data

---
+++ src/components/MedCard.js
@@ -0,0 +1,12 @@
+function MedCard() {
+	return (
+		<div>
+			<h4>Medication Name</h4>
+			<p>Dosage</p>
+			<p>Fewquency</p>
+			<p>last taken</p>
+		</div>
+	);
+}
+
+export default MedCard;
---
+++ src/pages/private/AllMeds.js
@@ -0,0 +1,12 @@
+import MedCard from "../../components/MedCard";
+
+function Medications() {
+	return (
+		<div>
+			<h2>MEdication List</h2>
+			<MedCard />
+		</div>
+	);
+}
+
+export default Medications;
