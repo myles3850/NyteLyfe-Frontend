b455bfa9e4986fb2709e9916fe518f428f8159d7
myles3850 <myles@browne.link>
2022-01-01 01:05:51 +0000

    removed supabase tests

--- src/pages/public/Home.js
+++ src/pages/public/Home.js
@@ -1,29 +1,8 @@
-import { useState } from "react";
-import { supabase } from "../../components/logic/Supabase";
-
-
 function Home() {
-	const UUID = "20cf2574-ec52-4f61-a0b9-841769a31aa0";
-
-	async function readData() {
-		let { data: Medication, error } = await supabase
-			.from("dosage_history")
-			.select("*");
-		console.log(Medication, error);
-	}
-
-	async function updateData() {
-		const { data, error } = await supabase
-			.from("Medication")
-			.insert([{ dose: 10, linked_user: UUID }]);
-			console.log(data);
-	}
 
 	return (
 		<div>
 			<h1>Pill Poppers Website</h1>
-			<button onClick={readData}>press to read data from DB</button>
-			<button onClick={updateData}>press to add data to DB</button>
 			<p>
 				Welcome to my new pill management page, here i will be managing
 				the pills i've taken in a format that works for those that say
