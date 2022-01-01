859a02f639123acd349d0956079dcf02968e8b33
myles3850 <myles@browne.link>
2022-01-01 04:39:21 +0000

    added alt nav bar for account area

--- src/components/interface/Navigation.js
+++ src/components/interface/Navigation.js
@@ -1,21 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import classes from "./Navigation.module.css";
 
 function Navigation() {
-	return (
-		<nav className={classes.Navigation}>
-			<div className={classes.dropdown_left}>
-				<h2 className={classes.dropbtn}>PillPoppers</h2>
-				<div className={classes.dropdown_content}>
-					<Link to="/">Home</Link>
-					<Link to="About">About</Link>
-					<Link to="creator">Creator</Link>
-					<Link to='account'>My Profile</Link>
+	let location = useLocation().pathname;
+
+	if (location.indexOf("/account") > -1) {
+		return (
+			<nav className={classes.Navigation}>
+				<div className={classes.dropdown}>
+					<Link to="/">
+						<h2 className={classes.dropbtn}>Pill Popperz</h2>
+					</Link>
+					<div className={classes.dropdown_content}>
+						<Link to="account">Home</Link>
+						<Link to="account/medications">Medication</Link>
+						<Link to="account/history">History</Link>
+						<Link to="account/settings">Settings</Link>
+					</div>
 				</div>
-			</div>
-		</nav>
-	);
+			</nav>
+		);
+	} else {
+		return (
+			<nav className={classes.Navigation}>
+				<div className={classes.dropdown}>
+					<Link to="/">
+						<h2 className={classes.dropbtn}>Pill Popperz</h2>
+					</Link>
+					<div className={classes.dropdown_content}>
+						<Link to="/">Home</Link>
+						<Link to="About">About</Link>
+						<Link to="creator">Creator</Link>
+						<Link to="account">Your Account</Link>
+					</div>
+				</div>
+			</nav>
+		);
+	}
 }
 
 export default Navigation;
