c50c7614c13e73545faa3d450bb90e68f4c45ac9
myles3850 <myles@browne.link>
2022-01-01 03:24:59 +0000

    enabled data from supabase to show in med card

--- src/components/private/MedCard.js
+++ src/components/private/MedCard.js
@@ -1,11 +1,14 @@
 import classes from "./MedCard.module.css";
 
-function MedCard() {
+function MedCard(prop) {
+
+	
+
 	return (
 		<div className={classes.card}>
 			<h4>Medication Name</h4>
 			<div className={classes.container}>
-				<p>Dosage</p>
+				<p>{prop.medication.dose}</p>
 				<p>Fewquency</p>
 				<p>last taken</p>
 			</div>
--- src/pages/private/AllMeds.js
+++ src/pages/private/AllMeds.js
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
+import { Supabase } from "../../components/logic/Supabase";
 import MedCard from "../../components/private/MedCard";
 
 function Medications() {
+
+	const [ data, setData ] = useState([]);
+	useEffect(() => {readData()}, []);
+
+
+	async function readData() {
+		const { data: Data, error } = await Supabase
+			.from("Medication")
+			.select("*");
+		setData(Data);
+	}
+
+
 	return (
 		<div>
 			<h2>Medication List</h2>
-			<MedCard />
-			<MedCard />
-			<MedCard />
+			{data.map((Medication) => { return <MedCard key={Medication.id} medication={Medication} />})}
 		</div>
 	);
 }
