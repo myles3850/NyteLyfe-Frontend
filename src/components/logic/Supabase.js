0972774d7d99f05906db353687d1109a54404c83
myles3850 <myles@browne.link>
2022-01-01 03:14:13 +0000

    renames export for supabase

--- src/components/logic/Supabase.js
+++ src/components/logic/Supabase.js
@@ -4,4 +4,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://mojptfpacmyjglncbykp.supabase.co";
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const Supabase = createClient(supabaseUrl, supabaseKey);
