6de7a3e75f14e1151b440267408a947e55e88edc
Myles Browne <myles@browne.link>
2022-01-15 15:55:04 +0000

    added new med button

--- src/pages/private/AllMeds.js
+++ src/pages/private/AllMeds.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Supabase } from "../../components/logic/Supabase";
 import MedCard from "../../components/private/MedCard";
 
 function Medications() {
 	const [data, setData] = useState([]);
+	let navigate = useNavigate();
+
 	useEffect(() => {
 		readData();
 	}, []);
@@ -18,7 +21,8 @@ function Medications() {
 			<h2>Medication List</h2>
 			{data.map((Medication) => {
 				return <MedCard key={Medication.id} medication={Medication} />;
-			})}
+			})} <br/>
+			<button onClick={() => navigate("new")}>Add medication</button>
 		</div>
 	);
 }
