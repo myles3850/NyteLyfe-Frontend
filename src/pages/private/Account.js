8f3a1aaa3afd846ccef53891595c675d0133beb4
Myles Browne <myles@browne.link>
2022-01-15 13:59:24 +0000

    added placeholder text for the account page

--- src/pages/private/Account.js
+++ src/pages/private/Account.js
@@ -1,38 +1,40 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { Supabase } from "../../components/logic/Supabase";
-import LoginScreen from "../auth/Login";
 import SignupComplete from "../auth/SignUpComplete";
 import AddMedication from "./AddMeds/MewMedication";
 import Medications from "./AllMeds";
 import UserSettings from "./UserSettings";
 
 function AccountPage() {
-	return <div>
-		<p>account home page</p>
-	</div>
+	return (
+		<div>
+			<h3>Account Home page</h3>
+			<p>
+				this page will contain links and infomration on quick stats for
+				your medication as well as a way to quickly mark them as taken
+			</p>
+		</div>
+	);
 }
 
-
-
-
 function Account() {
-
 	let navigate = useNavigate();
 
-	function handleSignOut () {
+	function handleSignOut() {
 		Supabase.auth.signOut();
 		navigate("/");
 	}
-	
 
 	return (
 		<div className="account_section">
 			<Routes>
-				<Route path='/' element={<Medications />}></Route>
+				<Route path="/" element={<AccountPage />}></Route>
 				<Route path="/medications/*" element={<Medications />} />
-				<Route path='/settings' element={<AddMedication />} />
-				<Route path="signupConfirmation/" element={<SignupComplete />} />
-
+				<Route path="/settings" element={<AddMedication />} />
+				<Route
+					path="signupConfirmation/"
+					element={<SignupComplete />}
+				/>
 			</Routes>
 			<button onClick={handleSignOut}>log off</button>
 		</div>
