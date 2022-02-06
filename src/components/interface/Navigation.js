4e380dbf34e2d8f6239488cf923e880b3810e50e
Myles Browne <myles@browne.link>
2022-02-06 16:29:21 +0000

    navbar rewrite for bootstrap

--- src/components/interface/Navigation.js
+++ src/components/interface/Navigation.js
@@ -1,3 +1,4 @@
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Supabase } from "../logic/Supabase";
 
@@ -5,38 +6,121 @@ import classes from "./Navigation.module.css";
 
 function Navigation() {
 	let location = useLocation().pathname;
+	let user = Supabase.auth.user();
+	let navigate = useNavigate();
+
+
+	function handleSignOut() {
+		Supabase.auth.signOut();
+		navigate("/");
+	}
+
 
 	if (location.indexOf("/account") > -1) {
 		return (
-			<nav className={classes.Navigation}>
-				<div className={classes.dropdown}>
-					<Link to="/">
-						<h2 className={classes.dropbtn}>Pill Popperz</h2>
-					</Link>
-					<div className={classes.dropdown_content}>
-						<Link to={Supabase.auth.user() ? "account" : "login"}>Home</Link>
-						<Link to={Supabase.auth.user() ? "account/medications" : "login"}>Medication</Link>
-						<Link to={Supabase.auth.user() ? "account/history" : "login"}>History</Link>
-						<Link to={Supabase.auth.user() ? "account/settings" : "login"}>Settings</Link>
-					</div>
-				</div>
-			</nav>
+			<Navbar expand="lg">
+				<Container fluid>
+					<Navbar.Brand as={Link} to="/">
+						Pillpopperz
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+							{user ? (
+								<Nav className="me-auto">
+									<Nav.Link as={Link} to="account">
+										Home
+									</Nav.Link>
+									<Nav.Link as={Link} to="account/medications">
+										Medications
+									</Nav.Link>
+									<Nav.Link as={Link} to="account/history">
+										Medication History
+									</Nav.Link>
+								</Nav>
+							) : (
+								<Nav className="me-auto">
+									<Nav.Link as={Link} to="/">
+										Home
+									</Nav.Link>
+									<Nav.Link as={Link} to="About">
+										About
+									</Nav.Link>
+									<Nav.Link as={Link} to="Creator">
+										Creater
+									</Nav.Link>
+								</Nav>
+							)}
+					</Navbar.Collapse>
+					<Navbar.Collapse className="justify-content-end">
+						<NavDropdown title={user ? user.email : "account"} id="basic-nav-dropdown" align="end">
+							{user ? (
+								<Container fluid>
+									<NavDropdown.Item as={Link} to="/account">
+										Account
+									</NavDropdown.Item>
+									<NavDropdown.Item as={Link} to='account/settings'>Settings</NavDropdown.Item>
+									<NavDropdown.Item onClick={handleSignOut}>Logout</NavDropdown.Item>
+								</Container>
+							) : (
+								<Container fluid>
+									<NavDropdown.Item as={Link} to="login">
+										Login
+									</NavDropdown.Item>
+									<NavDropdown.Item as={Link} to="signup">
+										Register
+									</NavDropdown.Item>
+								</Container>
+							)}
+						</NavDropdown>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
 		);
 	} else {
 		return (
-			<nav className={classes.Navigation}>
-				<div className={classes.dropdown}>
-					<Link to="/">
-						<h2 className={classes.dropbtn}>Pill Popperz</h2>
-					</Link>
-					<div className={classes.dropdown_content}>
-						<Link to="/">Home</Link>
-						<Link to="About">About</Link>
-						<Link to="creator">Creator</Link>
-						<Link to={Supabase.auth.user() ? "account" : "login"}>Your Account</Link>
-					</div>
-				</div>
-			</nav>
+			<Navbar expand="lg">
+				<Container fluid>
+					<Navbar.Brand as={Link} to="/">
+						Pillpopperz
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Nav.Link as={Link} to="/">
+								Home
+							</Nav.Link>
+							<Nav.Link as={Link} to="About">
+								About
+							</Nav.Link>
+							<Nav.Link as={Link} to="Creator">
+								Creater
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+					<Navbar.Collapse className="justify-content-end">
+						<NavDropdown title={user ? user.email : "account"} id="basic-nav-dropdown" align="end">
+							{user ? (
+								<Container fluid>
+									<NavDropdown.Item as={Link} to="/account">
+										Account
+									</NavDropdown.Item>
+									<NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
+									<NavDropdown.Item href="#action/3.3">Logout</NavDropdown.Item>
+								</Container>
+							) : (
+								<Container fluid>
+									<NavDropdown.Item as={Link} to="login">
+										Login
+									</NavDropdown.Item>
+									<NavDropdown.Item as={Link} to="signup">
+										Register
+									</NavDropdown.Item>
+								</Container>
+							)}
+						</NavDropdown>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
 		);
 	}
 }
