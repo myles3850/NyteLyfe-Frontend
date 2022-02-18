3ce06040cda9456323e44207c6a9fe8695be0863
Myles Browne <myles@browne.link>
2022-02-18 20:15:24 +0000

    enabled form submission and added form fields

--- src/pages/private/Recording/RecordTaken.jsx
+++ src/pages/private/Recording/RecordTaken.jsx
@@ -1,50 +1,53 @@
 import { useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { Button, Container, Form, Nav } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+import { Button, Container, Form } from "react-bootstrap";
 import DatePicker from "react-date-picker";
 import TimePicker from "react-time-picker";
 import { Supabase } from "../../../components/logic/Supabase";
+
 export default function FullMedRecording() {
 	const [dateValue, changeDate] = useState(new Date());
 	const [timeValue, changeTime] = useState(new Date());
+	const [skipped, setSkipped] = useState(false);
 
-	let navigate = useNavigate();
+	const navigate = useNavigate();
 	const { medicationId } = useParams();
 
-	function TestSubmit(e) {
+	async function HandleSubmit(e) {
 		e.preventDefault();
-		let error = { message: "working" };
 		const user = Supabase.auth.user();
-		console.log(dateValue, timeValue, user.id, medicationId);
-		!error ? navigate("/account", { replace: true }) : alert(error.message);
-	}
-
-	async function HandleSubmit() {
-		const user = Supabase.auth.user();
+		console.log(e);
 
 		const { data, error } = await Supabase.from("dosage_history").insert([
-			{ id: medicationId, user_taken_id: user.id, date_taken: dateValue, time_taken: timeValue, skipped: false },
+			{ medication_taken_id: medicationId, date_taken: dateValue, time_taken: timeValue, skipped: skipped },
 		]);
+		!error ? navigate("/account", { replace: true }) : alert(error.message);
 	}
 
 	return (
 		<Container fluid>
-			<Nav.Link as={Link} to="/account">
+			<Button
+				onClick={() => {
+					navigate("/account");
+				}}
+			>
 				&lt; Back
-			</Nav.Link>
-			<p>lets report what we doook</p>
-			<Form method="post" onSubmit={TestSubmit}>
-				<Form.Group>
-					<Form.Label>Date</Form.Label> <br />
-					<DatePicker onChange={changeDate} value={dateValue} />
-				</Form.Group>
-				<Form.Group>
-					<Form.Label>Time</Form.Label> <br />
-					<TimePicker onChange={changeTime} value={timeValue} disableClock="True" format="hh:mm a" />
-				</Form.Group>{" "}
-				<br />
-				<Button type="submit">Submit</Button>
-			</Form>
+			</Button>
+			<Container fluid className="text-center">
+				<p>lets report what we doook</p>
+				<Form method="post" onSubmit={HandleSubmit}>
+					<Form.Group>
+						<Form.Label>Date</Form.Label> <br />
+						<DatePicker onChange={changeDate} value={dateValue} />
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Time</Form.Label> <br />
+						<TimePicker onChange={changeTime} value={timeValue} disableClock="True" format="hh:mm a" />
+					</Form.Group>
+					<br />
+					<Button type="submit">Submit</Button>
+				</Form>
+			</Container>
 		</Container>
 	);
 }
