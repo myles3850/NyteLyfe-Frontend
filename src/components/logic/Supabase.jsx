3d320d0134dbbf5ffa64f9338f0f5ffc7d13ed9a
Myles Browne <myles@browne.link>
2022-04-09 23:09:29 +0100

    temp moved env variables to file- will add doppler

--- src/components/logic/Supabase.jsx
+++ src/components/logic/Supabase.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://mojptfpacmyjglncbykp.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDk3OTgyOCwiZXhwIjoxOTU2NTU1ODI4fQ.LqZYa9hT6j5VCmP46VFv8Y5ceeGP4iDOkgM1a7oM3mI";
 
 export const Supabase = createClient(supabaseUrl, supabaseKey);
