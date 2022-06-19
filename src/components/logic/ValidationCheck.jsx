4d991806d9e29fac5a4099ae6ecb7ea3c2c97065
myles browne <myles@browne.link>
2022-06-19 14:59:27 +0100

    correcteed tracked files

--- src/components/logic/ValidationCheck.jsx
+++
@@ -1,7 +0,0 @@
-import { useNavigate } from "react-router-dom";
-let navigate = useNavigate();
-
-export default function HandleErrorCheck(error, redirect) {
-
-	!error ? navigate(redirect, { replace: true }) : alert(error.message);
-}
--- src/pages/auth/SignUpComplete.jsx
+++
@@ -1,9 +0,0 @@
-function SignupComplete() {
-	return (
-		<div>
-			<p>Weve sent you a registration link. head over to your email to confirm your account</p>
-		</div>
-	);
-}
-
-export default SignupComplete;
